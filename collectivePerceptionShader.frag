cbc1982224ac1c7195124eb47702d9e84b097e4e
TommyMcPhee <135555457+TommyMcPhee@users.noreply.github.com>
2024-02-18 14:28:19 -0700

    Add files via upload

--- collectivePerceptionShader.frag
+++ collectivePerceptionShader.frag
@@ -1,26 +1,17 @@
 #version 150
 
-uniform sampler2DRect tex0;
-//uniform sampler2DRect tex1;
-//uniform sampler2DRect tex2;
-//uniform sampler2DRect tex3;
-in vec2 texCoordVarying;
 out vec4 outputColor;
 
 uniform vec2 window;
-uniform vec3 frequency;
 uniform vec3 index;
-uniform vec3 ring;
-uniform vec3 color;
-
-uniform vec4 z0;
-//uniform vec4 z1;
-//uniform vec4 z2;
-//uniform vec4 z3;
-
-vec2 normalized;
-
-float pixels;
+uniform vec3 frequency;
+uniform vec3 amplitude;
+uniform vec3 x;
+uniform vec3 y;
+uniform vec4 ab;
+uniform vec4 ac;
+uniform vec4 bc;
+uniform vec2 abc;
 
 float unipolar(float input){
     return input * 0.5 + 0.5;
@@ -30,7 +21,7 @@ float bipolar(float input){
     return (input - 0.5) * 2.0;
 }
 
-float oscillate(vec2 twoPiNormalized, float _frequency, float _index, float _ring, float _amplitude){
+float oscillate(vec2 twoPiNormalized, float _frequency, float _index, float _amplitude){
     float phaseX = _frequency * twoPiNormalized.x;
     float phaseY = _frequency * twoPiNormalized.y;
     float modulatorX = cos(phaseX) * _index;
@@ -40,68 +31,30 @@ float oscillate(vec2 twoPiNormalized, float _frequency, float _index, float _rin
     float carrierX = unipolar(cos(phaseX * carrierFrequencyX));
     float carrierY = unipolar(cos(phaseY * carrierFrequencyY));
     float mixedOutput = mix(carrierX, carrierY, 0.5);
-    float ringOutput = carrierX * carrierY;
-    return mix(mixedOutput, ringOutput, _ring) * _amplitude;
+    return mixedOutput * _amplitude;
 }
 
-vec2 translate(vec2 texCoord, vec2 window, vec4 translation){
-    return vec2((translation.x * window.x + texCoord.x) * translation.z, (translation.y * window.y + texCoord.y) * translation.w);
+vec2 modulate(float a, float b, float index){
+    return mix(vec2(a, b), vec2(a * b), index);
 }
 
-/*
-uniform vec2 oscillators;
-uniform vec4 color;
-uniform vec4 translate;
-
-float powerScale(float input){
-    float scale = pow(pixels, 0.25);
-    return pow(input, 1.0 + pow(scale, 1.0 / pow(scale, 0.5))) * scale;
+float distribute(vec2 xy, vec2 normalized){
+    return mix(mix(1.0 - normalized.x, normalized.x, xy.x), mix(1.0 - normalized.y, normalized.y, xy.y), 0.5);
 }
 
-float scalePhase(float phase, float skew){
-    float scaledSkew = powerScale(skew);
-    return pow(phase, scaledSkew) * TWO_PI;
-}
-
-float scaleTension(float tension){
-    return pow(tension, 2.0) * 2.0;
-}
-
-float lfo(float phase, float skew, float depth, float offset, float tension){
-    float scaledOffset = offset * TWO_PI;
-    float scaledPhase = scalePhase(phase, skew);
-    float sine = sin(scaledPhase + scaledOffset);
-    float scaledTension = scaleTension(tension);
-    float scaledDepth = powerScale(depth);
-    return pow(unipolar(abs(sine) * sign(sine)), scaledDepth);
-}
-
-float oscillate(float phase, float skew, float depth, float frequency, float tension){
-    float scaledPhase = scalePhase(phase, skew);
-    float scaledFrequency = pow(frequency, 0.5) * pixels;
-    float sine = sin(scaledPhase * TWO_PI * frequency);
-    float scaledTension = scaleTension(tension);
-    float scaledDepth = powerScale(depth);
-    return pow(unipolar(abs(sine) * sign(sine)), scaledDepth);
-}
-
-float averageTwo(float a, float b){
-    return (a + b) / 2.0;
-}
-
-float normalizedAdd(float a, float b){
-    return (a + b) / (1.0 + b);
-}
-*/
 void main()
-{
-    vec4 buffer0 = texture2DRect(tex0, translate(texCoordVarying, window, z0));   
+{ 
     vec2 normalized = gl_FragCoord.xy / window;
-    //pixels = window.x * window.y;
     float pi = radians(180.0);
     vec2 twoPiNormalized = normalized * 2.0 * pi - vec2(pi, pi);
-    float red = oscillate(twoPiNormalized, frequency.r, index.r, ring.r, color.r);
-    float green = oscillate(twoPiNormalized, frequency.g, index.g, ring.g, color.g);
-    float blue = oscillate(twoPiNormalized, frequency.b, index.b, ring.b, color.b);
-    outputColor = mix(vec4(red, green, blue, 1.0), buffer0, 0.33);
+    float red = oscillate(twoPiNormalized, frequency.r, index.r, amplitude.r) * distribute(vec2(x.r, y.r), normalized);
+    float green = oscillate(twoPiNormalized, frequency.g, index.g, amplitude.g) * distribute(vec2(x.g, y.g), normalized);
+    float blue = oscillate(twoPiNormalized, frequency.b, index.b, amplitude.b) * distribute(vec2(x.g, y.b), normalized);
+    vec2 redGreen = modulate(red, green, ab.z) * distribute(ab.xy, normalized) * ab.w;
+    vec2 redBlue = modulate(red, blue, ac.z) * distribute(ac.xy, normalized) * ac.w;
+    vec2 greenBlue = modulate(green, blue, bc.z) * distribute(bc.xy, normalized) * bc.w;
+    vec3 color = vec3(mix(redGreen.x, redBlue.x, 0.5), mix(redGreen.y, greenBlue.x, 0.5), mix(redBlue.y, greenBlue.y, 0.5)) * distribute(abc, normalized);
+    outputColor = vec4(newColor, 1.0);
+    
+    //outputColor = vec4(1.0, 0.0, 0.0, 1.0);
 }
--- ofApp.cpp
+++ ofApp.cpp
@@ -15,15 +15,19 @@ void ofApp::audioSetup() {
 	settings.setApi(ofSoundDevice::Api::MS_DS);
 	stream.setup(settings);
 	nyquist = (float)sampleRate * 0.5;
-	frequencyIncrement = 10.0 / nyquist;
+	frequencyIncrement = 100.0 / nyquist;
+	lowFrequencyIncrement = frequencyIncrement;
 	fundamentalFrequency = (float)frameRate / 9.0;
 	frequencyA = fundamentalFrequency;
 	frequencyB = fundamentalFrequency * 4.0;
 	frequencyC = fundamentalFrequency * 7.0;
+	lowFrequencyA = frequencyA;
+	lowFrequencyB = frequencyB;
+	lowFrequencyC = frequencyC;
 	ratio = 9.0;
-	redLFO = sinOsc(fundamentalFrequency / 40.0, 0.0, 1.0, sampleRate);
-	greenLFO = sinOsc(fundamentalFrequency / 70.0, 0.0, 1.0, sampleRate);
-	blueLFO = sinOsc(fundamentalFrequency / 100.0, 0.0, 1.0, sampleRate);
+	redLFO = sinOsc(fundamentalFrequency / 100.0, 0.0, 1.0, sampleRate);
+	greenLFO = sinOsc(fundamentalFrequency * 4.0 / 100.0, 0.0, 1.0, sampleRate);
+	blueLFO = sinOsc(fundamentalFrequency * 7.0/ 100.0, 0.0, 1.0, sampleRate);
 	modulatorA = sinOsc(frequencyA * ratio, 0.0, 1.0, sampleRate);
 	modulatorB = sinOsc(frequencyB * ratio * 4.0, 0.0, 1.0, sampleRate);
 	modulatorC = sinOsc(frequencyC * ratio, 0.0, 1.0, sampleRate);
@@ -42,7 +46,7 @@ void ofApp::ofSoundStreamSetup(ofSoundStreamSettings &settings) {
 }
 
 void ofApp::videoSetup() {
-	ofSetFrameRate(60);
+	ofSetFrameRate(frameRate);
 	shader.load("collectivePerceptionShader");
 	videoBuffer.allocate(ofGetScreenWidth(), ofGetScreenHeight());
 	videoBuffer.clear();
@@ -52,47 +56,129 @@ void ofApp::videoSetup() {
 }
 
 void ofApp::audioOut(ofSoundBuffer& audioBuffer) {
+	cout << lowFrequencyIncrement << endl;
 	for (int a = 0; a < audioBuffer.getNumFrames(); a++) {
 		renderSample();
 		for (int b = 0; b < 2; b++) {
 			audioBuffer[a * 2 + b] = sample[b];
+			lastSample[b] = sample[b];
 		}
 	}
 }
 
 void ofApp::renderSample() {
-	redSample = unipolar(redLFO.getSample());
-	greenSample = unipolar(greenLFO.getSample());
-	blueSample = unipolar(blueLFO.getSample());
-	indexA = pow(nyquist - frequencyA, redSample * frequencyA / nyquist);
-	indexB = pow(nyquist - frequencyB, greenSample * frequencyB / nyquist);
-	indexC = pow(nyquist - frequencyC, blueSample * frequencyC / nyquist);
+	lowFrequencyA -= lowFrequencyIncrement;
+	lowFrequencyB -= lowFrequencyIncrement;
+	lowFrequencyC -= lowFrequencyIncrement;
 	frequencyA += frequencyIncrement;
 	frequencyB += frequencyIncrement;
 	frequencyC += frequencyIncrement;
+	/*
 	if (frequencyA > nyquist) {
+		cout << "done" << endl;
 		exit();
 	}
+	*/
+	redLFO.setFreq(lowFrequencyA);
+	greenLFO.setFreq(lowFrequencyB);
+	blueLFO.setFreq(lowFrequencyC);
+	redSample = unipolar(redLFO.getSample());
+	greenSample = unipolar(greenLFO.getSample());
+	blueSample = unipolar(blueLFO.getSample());
+	//
+	modulatorAAmplitude = pow(nyquist - frequencyA, averageTwo(greenSample, blueSample) * frequencyA / nyquist);
+	modulatorBAmplitude = pow(nyquist - frequencyB, averageTwo(redSample, blueSample) * frequencyB / nyquist);
+	modulatorCAmplitude = pow(nyquist - frequencyC, averageTwo(redSample, greenSample) * frequencyC / nyquist);
+	//
+	modulatorAPan = redSample;
+	modulatorBPan = greenSample;
+	modulatorCPan = blueSample;
+	//
+		
+	carrierAPan = averageTwo(greenSample, blueSample);
+	carrierBPan = averageTwo(redSample, blueSample);
+	carrierCPan = averageTwo(redSample, greenSample);
+	carrierAAmplitude = abs(greenSample - blueSample);
+	carrierBAmplitude = abs(redSample - blueSample);
+	carrierCAmplitude = abs(redSample - greenSample);
+	abRing = averageTwo(1.0 - (redSample * greenSample), blueSample);
+	acRing = averageTwo(1.0 - (redSample * blueSample), greenSample);
+	bcRing = averageTwo(1.0 - (greenSample * blueSample), redSample);
+	abRingPan = abs(averageTwo(redSample, greenSample) - blueSample);
+	acRingPan = abs(averageTwo(redSample, blueSample) - greenSample);
+	bcRingPan = abs(averageTwo(greenSample, blueSample) - redSample);
+	abAmplitude = abs(redSample * greenSample - blueSample);
+	acAmplitude = abs(redSample * blueSample - greenSample);
+	bcAmplitude = abs(greenSample * blueSample - redSample);
+	abPan = averageTwo(abs(redSample - greenSample), blueSample);
+	acPan = averageTwo(abs(redSample - blueSample), greenSample);
+	bcPan = averageTwo(abs(greenSample - blueSample), redSample);
+	ring = 1.0 - (redSample * greenSample * blueSample);
+	ringPan = averageThree(redSample, greenSample, blueSample);
 	modulatorA.setFreq(frequencyA * ratio);
 	modulatorB.setFreq(frequencyB * ratio);
 	modulatorC.setFreq(frequencyC * ratio);
 	modulatorASample = modulatorA.getSample();
 	modulatorBSample = modulatorB.getSample();
 	modulatorCSample = modulatorC.getSample();
-	carrierA.setFreq(modulatorASample * frequencyA * indexA + frequencyA);
-	carrierB.setFreq(modulatorBSample * frequencyB * indexB + frequencyB);
-	carrierC.setFreq(modulatorCSample * frequencyC * indexC + frequencyC);
-	carrierASample = carrierA.getSample();
-	carrierBSample = carrierB.getSample();
-	carrierCSample = carrierC.getSample();
+	indexA[0] = left(modulatorAAmplitude, modulatorAPan);
+	indexA[1] = right(modulatorAAmplitude, modulatorAPan);
+	indexB[0] = left(modulatorBAmplitude, modulatorBPan);
+	indexB[1] = right(modulatorBAmplitude, modulatorBPan);
+	indexC[0] = left(modulatorCAmplitude, modulatorCPan);
+	indexC[1] = right(modulatorCAmplitude, modulatorCPan);
 	for (int b = 0; b < 2; b++) {
-		//sample[b] = (carrierASample + carrierBSample + carrierCSample) / 3.0;
-		sample[b] = carrierASample * carrierBSample * carrierCSample;
+		carrierA[b].setAmp(carrierAAmplitude);
+		carrierB[b].setAmp(carrierBAmplitude);
+		carrierC[b].setAmp(carrierCAmplitude);
+		carrierA[b].setFreq(modulatorASample * frequencyA * indexA[b] + frequencyA);
+		carrierB[b].setFreq(modulatorBSample * frequencyB * indexB[b] + frequencyB);
+		carrierC[b].setFreq(modulatorCSample * frequencyC * indexC[b] + frequencyC);
+		carrierASample[b] = carrierA[b].getSample();
+		carrierBSample[b] = carrierB[b].getSample();
+		carrierCSample[b] = carrierC[b].getSample();
 	}
+	sampleA[0] = left(carrierASample[0], carrierAPan);
+	sampleA[1] = right(carrierASample[1], carrierAPan);
+	sampleB[0] = left(carrierBSample[0], carrierBPan);
+	sampleB[1] = right(carrierBSample[0], carrierBPan);
+	sampleC[0] = left(carrierCSample[0], carrierCPan);
+	sampleAB[0] = modulateTwo(carrierASample[0], carrierBSample[0], left(abRing, abRingPan), left(abAmplitude, abPan));
+	sampleAB[1] = modulateTwo(carrierASample[1], carrierBSample[1], right(abRing, abRingPan), right(abAmplitude, abPan));
+	sampleAC[0] = modulateTwo(carrierASample[0], carrierCSample[0], left(acRing, acRingPan), left(acAmplitude, acPan));
+	sampleAC[1] = modulateTwo(carrierASample[1], carrierCSample[1], right(acRing, acRingPan), right(acAmplitude, acPan));
+	sampleBC[0] = modulateTwo(carrierBSample[0], carrierCSample[0], left(bcRing, bcRingPan), left(bcAmplitude, bcPan));
+	sampleBC[1] = modulateTwo(carrierBSample[1], carrierCSample[1], right(bcRing, bcRingPan), right(bcAmplitude, bcPan));
+	sample[0] = modulateTwo(modulateThree(sampleAB[0], sampleAC[0], sampleBC[0], left(ring, ringPan)), sample[0], left(filterRing, filterRingPan), left(filterAmplitude, filterPan));
+	sample[1] = modulateTwo(modulateThree(sampleAB[1], sampleAC[1], sampleBC[1], right(ring, ringPan)), sample[1], left(filterRing, filterRingPan), left(filterAmplitude, filterPan));
 }
 
 inline float ofApp::unipolar(float input) {
 	return input * 0.5 + 0.5;
+}
+
+inline float ofApp::left(float input, float pan) {
+	return sqrt(1.0 - pan) * input;
+}
+
+inline float ofApp::right(float input, float pan) {
+	return sqrt(pan) * input;
+}
+
+inline float ofApp::modulateTwo(float inputA, float inputB, float ring, float amplitude) {
+	return amplitude * (inputA * inputB * ring + averageTwo(inputA, inputB)) / (1.0 + (1.0 - ring));
+}
+
+inline float ofApp::modulateThree(float inputA, float inputB, float inputC, float ring) {
+	return inputA * inputB * inputC * ring + averageThree(inputA, inputB, inputC) / (1.0 + (1.0 - ring));
+}
+
+inline float ofApp::averageTwo(float inputA, float inputB) {
+	return (inputA + inputB) / 2.0;
+}
+
+inline float ofApp::averageThree(float inputA, float inputB, float inputC) {
+	return (inputA + inputB + inputC) / 2.0;
 }
 
 //--------------------------------------------------------------
@@ -105,7 +191,6 @@ void ofApp::draw(){
 	shader.end();
 	videoBuffer.end();
 	videoBuffer.draw(0, 0);
-	//cout << frequencyA << endl;
 }
 
 void ofApp::refresh() {
@@ -118,14 +203,12 @@ void ofApp::refresh() {
 
 void ofApp::setUniforms() {
 	shader.setUniform2f("window", window);
-	index.set(indexA, indexB, indexC);
+	index.set(modulatorAAmplitude, modulatorBAmplitude, modulatorCAmplitude);
 	shader.setUniform3f("index", index);
 	frequency.set(scaleFrequency(frequencyA), scaleFrequency(frequencyB), scaleFrequency(frequencyC));
 	shader.setUniform3f("frequency", frequency);
-	color.set(redSample, greenSample, blueSample);
-	shader.setUniform3f("color", color);
-	z0.set(0.0, 0.0, 1.0, 1.0);
-	shader.setUniform4f("z0", z0);
+	amplitude.set(redSample, greenSample, blueSample);
+	shader.setUniform3f("amplitude", amplitude);
 }
 
 float ofApp::scaleFrequency(float input) {
